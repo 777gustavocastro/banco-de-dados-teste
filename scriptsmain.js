bc95502a89ef3e6bff727337ea190d2a7c58e071
777gustavocastro <141282660+777gustavocastro@users.noreply.github.com>
2024-11-28 21:49:04 -0300

    banco de dados teste

---
+++ scriptsmain.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("data/empresas.json")
+    .then((response) => response.json())
+    .then((data) => renderEmpresas(data));
+});
+
+function renderEmpresas(empresas) {
+  const empresaList = document.getElementById("empresa-list");
+  empresaList.innerHTML = empresas
+    .map(
+      (empresa) => `
+      <div class="card">
+        <img src="images/${empresa.logo}" alt="Logo de ${empresa.nome}">
+        <h2>${empresa.nome}</h2>
+        <p>${empresa.local}</p>
+        <p><strong>Setor:</strong> ${empresa.setor}</p>
+        <p><strong>Contato:</strong> ${empresa.contato}</p>
+        <div class="status">
+          <div class="status-circle ${
+            empresa.ativo ? "status-online" : "status-offline"
+          }"></div>
+          <span>${empresa.ativo ? "No ar" : "Inativa"}</span>
+        </div>
+      </div>
+    `
+    )
+    .join("");
+}
